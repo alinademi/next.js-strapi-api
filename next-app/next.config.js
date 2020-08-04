606aec65520a188582ffa8d110025d58ece1e12a
ali <nademi@gmail.com>
2020-08-03 18:18:20 -0700

    dotenv added and API_URL was defined in neex.config.js and dotenv

--- next-app/next.config.js
+++ next-app/next.config.js
@@ -1,10 +1,15 @@
-const path = require('path')
+const path = require("path");
+require("dotenv").config();
 
 module.exports = {
-    webpack: config => {
-        config.resolve.alias['components'] = path.join(__dirname, 'components')
-        config.resolve.alias['public'] = path.join(__dirname, 'public')
+  env: {
+    API_URL: process.env.API_URL,
+  },
 
-        return config
-    }
-}
+  webpack: (config) => {
+    config.resolve.alias["components"] = path.join(__dirname, "components");
+    config.resolve.alias["public"] = path.join(__dirname, "public");
+
+    return config;
+  },
+};
